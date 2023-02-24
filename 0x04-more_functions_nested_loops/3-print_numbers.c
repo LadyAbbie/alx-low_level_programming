b3f0c20f2b0d125fd9ebf9a7b52de7ef62272533
LadyAbbie <abbieasa9@gmail.com>
2023-02-23 17:02:34 -0800

    numbers speak

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9
+ * print_numbers - prints numbers 0-9
  *
- * Returns: void
-*/
+ * Return: void
+ */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != '2' && i != '4')
-		{
-			_putchar(i);
-		}
+		_putchar(c);
 	}
 	_putchar('\n');
 }
