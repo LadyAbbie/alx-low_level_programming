1d9110962278c55ceea38a49738b5d61fcc66690
LadyAbbie <abbieasa9@gmail.com>
2023-03-23 21:33:28 -0700

    ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,33 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - compute sum of parameters
- * @n: number of parameters
- * Return: sum (integer)
+ * sum_them_all - entry point
+ * Description: returns the sum of all its parameters
+ * @n: the number of parameters to sum
+ * Return: the sum of all the parameters, or 0 if n == 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	/* declare a variable of type va_list to store the parameters */
 	va_list args;
-	int sum;
+	int sum = 0;
 	unsigned int i;
 
 	if (n == 0)
+	{
 		return (0);
+	}
 
+	/* initialize the va_list with the first parameter */
 	va_start(args, n);
-	sum = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
+		/* add each parameter to the sum */
 	}
+	/* clean up the va_list */
 	va_end(args);
-
 	return (sum);
 }
